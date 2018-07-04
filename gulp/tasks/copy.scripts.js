dc714e4c32912b5a2c59da208ff7ccae02e8e1a2
Dmitry Kovalev <mr.raccoon.dev@gmail.com>
2018-07-04 12:24:24 +0300

    Revert "add showcase page, add area management page"

--- gulp/tasks/copy.scripts.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function() {
-  $.gulp.task('copy:scripts', function() {
-    return $.gulp.src('./source/scripts/**/*.*', { since: $.gulp.lastRun('copy:scripts') })
-      .pipe($.gulp.dest($.config.root + '/scripts'))
-      .pipe($.browserSync.stream());
-  });
-};
